0786448846d0032a795bcce4468d5cf31f6beb6d
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-03-30 16:36:19 +0200

    Now the linearized version gives a somewhat similar result to the nonlinear version

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ import danaila_natural_convection
 
 #danaila_natural_convection.run(mesh_M=20, time_step_size=1.e-4, output_dir='M20')
 
-danaila_natural_convection.run(mesh_M=40, time_step_size=1.e-4, output_dir='M40')
+#danaila_natural_convection.run(mesh_M=40, time_step_size=1.e-4, output_dir='M40')
 
-#danaila_natural_convection.run(linearize=True, mesh_M=20, time_step_size=1.e-2, output_dir='linearized_M20')
+danaila_natural_convection.run(linearize=True, mesh_M=20, time_step_size=1.e-3, output_dir='linearized_M20')
 
 #danaila_natural_convection.run(mesh_M=20, time_step_size=1.e-3, final_time=0., output_dir='t0_M20')
 
