aae53cfb5980f4a312adc2186e4ae3b57c2e47ea
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-04-13 17:54:24 +0200

    Append to previous commit

--- test_danaila_natural_convection.py
+++
@@ -1,5 +0,0 @@
-import danaila_natural_convection
-
-# For this case, the Neumann BC seems to hold for a while, and then it destabilizes and breaks.
-danaila_natural_convection.run(mesh_M=20, time_step_size=1.e-3, final_time=1., output_dir='output/danaila_natural_convection')
-
