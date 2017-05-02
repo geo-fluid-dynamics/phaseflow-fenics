ec0e8e93043ba666dccafed73a8ca8a3842b37e0
Alexander G Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-05-02 12:29:39 +0200

    Removed redundant scripts

--- test_heat.py
+++
@@ -1,4 +0,0 @@
-import natural_convection
-
-
-natural_convection.run(g=(0., 0.), linearize=True, initial_mesh_M=4, adaptive_time=True, time_step_size=1.e-3, final_time=1., stop_when_steady=True, output_dir='output/linearized_heat_adaptive_M4')
--- test_steady_lid_driven_cavity.py
+++
@@ -1,4 +0,0 @@
-import steady_lid_driven_cavity
-
-# For this case, the Neumann BC seems to hold for a while, and then it destabilizes and breaks.
-steady_lid_driven_cavity.run(mesh_M=16, output_dir='output/steady_lid_driven_cavity_M16')
