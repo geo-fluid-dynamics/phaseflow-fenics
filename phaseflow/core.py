445d9c7e154fe1b649b2262f772994faf454012b
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-05-19 15:52:05 +0200

    Moved arguments() definition to helpers

--- phaseflow/core.py
+++ phaseflow/core.py
@@ -21,18 +21,6 @@ import dolfin
 import helpers
 import time
     
-    
-def arguments():
-    """Returns tuple containing dictionary of calling function's
-       named arguments and a list of calling function's unnamed
-       positional arguments.
-    """
-    from inspect import getargvalues, stack
-    posname, kwname, args = getargvalues(stack()[1][0])[-3:]
-    posargs = args.pop(posname, [])
-    args.update(args.pop(kwname, []))
-    return args, posargs
-
  
 Re = 1.
 
@@ -83,7 +71,7 @@ def run(
     #
     print("Running Phaseflow with the following arguments:")
     
-    print(arguments())
+    print(helpers.arguments())
     
     # Validate inputs    
     if type(time_step_bounds) == type(1.):
--- phaseflow/helpers.py
+++ phaseflow/helpers.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 class BoundedValue(object):
 
     def __init__(self, min=0., value=0., max=0.):
@@ -20,3 +23,19 @@ class BoundedValue(object):
             value = self.min
             
         self.value = value
+        
+
+def arguments():
+    """Returns tuple containing dictionary of calling function's
+       named arguments and a list of calling function's unnamed
+       positional arguments.
+    """
+    
+    
+    posname, kwname, args = inspect.getargvalues(inspect.stack()[1][0])[-3:]
+    
+    posargs = args.pop(posname, [])
+    
+    args.update(args.pop(kwname, []))
+    
+    return args, posargs
