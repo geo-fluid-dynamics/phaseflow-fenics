8ebd3236595674ee251c81ebb2ec58065fca65f4
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-05-19 15:22:57 +0200

    Made formatting consistent

--- phaseflow/time.py
+++ phaseflow/time.py
@@ -4,8 +4,10 @@ import dolfin
 class TimeStepSize(helpers.BoundedValue):
 
     def __init__(self, bounded_value):
+    
         super(TimeStepSize, self).__init__(bounded_value.min, bounded_value.value, bounded_value.max)
 
+        
     def set(self, value):
     
         old_value = self.value
@@ -13,4 +15,5 @@ class TimeStepSize(helpers.BoundedValue):
         super(TimeStepSize, self).set(value)
         
         if abs(self.value - old_value) > dolfin.DOLFIN_EPS:
+        
             print 'Set time step size to dt = ' + str(value)
