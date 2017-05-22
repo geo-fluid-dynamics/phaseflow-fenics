b431f1cded234a5d3385a5768eb4c1b9404aba5c
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2017-05-22 11:51:20 +0200

    Append to previous commit

---
+++ phaseflow/output.py
@@ -0,0 +1,15 @@
+def write_solution(solution_files, _w, current_time):
+
+        w = _w.leaf_node()
+        
+        velocity, pressure, temperature = w.split()
+        
+        velocity.rename("u", "velocity")
+        
+        pressure.rename("p", "pressure")
+        
+        temperature.rename("theta", "temperature")
+        
+        for i, var in enumerate([velocity, pressure, temperature]):
+        
+            solution_files[i] << (var, current_time) 
