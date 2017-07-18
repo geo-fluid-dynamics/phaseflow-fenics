ce791b2d29cfa3a22d74de627eb517f6d13b71b6
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-07-18 10:56:16 +0200

    Reduced cost of Stefan Problem test, added rigorous verification, and added examples directory for expensive examples that shouldn't be in the automated test suite

--- examples/stefan_problem.py
+++ examples/stefan_problem.py
@@ -1,24 +1,8 @@
 from .context import phaseflow
 
 import fenics
-import scipy.optimize as opt
 
 
-def verify_pci_position(w):
-
-    true_pci_pos = 0.075551957640682 # From Kai's MATLAB script
-    
-    def theta(x):
-    
-        wval = w(fenics.Point(x))
-        
-        return wval[2]
-    
-    pci_pos = opt.newton(theta, 0.075)
-    
-    assert(abs(pci_pos - true_pci_pos) < 0.002)
-        
-        
 def test_stefan_problem():
 
     theta_h = 1.
@@ -26,7 +10,8 @@ def test_stefan_problem():
     theta_c = -1.
     
     w = phaseflow.run(
-        write_output = False,
+        output_dir = 'output/test_stefan_problem/',
+        output_format = 'table',
         Pr = 1.,
         Ste = 1.,
         g = [0.],
@@ -40,12 +25,10 @@ def test_stefan_problem():
             {'subspace': 2, 'value_expression': theta_h, 'degree': 2, 'location_expression': "near(x[0],  0.)", 'method': "topological"},
             {'subspace': 2, 'value_expression': theta_c, 'degree': 2, 'location_expression': "near(x[0],  1.)", 'method': "topological"}],
         regularization = {'a_s': 2., 'theta_s': 0.01, 'R_s': 0.005},
-        final_time = 0.01,
+        final_time = 1.,
         time_step_bounds = 0.001,
         linearize = False)
 
-    verify_pci_position(w)
-    
 
 if __name__=='__main__':
     
