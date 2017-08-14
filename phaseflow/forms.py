49eb55df702dac6571fea46ec3aee2c050fc83b4
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-08-14 14:36:20 +0200

    In work: This runs, but the solution no longer is close enough to the known solution. Also it runs quite slowly. I want to try not using the fenics.derivative method

--- phaseflow/forms.py
+++ phaseflow/forms.py
@@ -41,15 +41,19 @@ class FormFactory():
         self.Re = fenics.Constant(globals.Re)
     
     
-    def make_nonlinear_form(self, dt, w, w_n):
+    def make_nonlinear_form(self, dt, w_n):
     
         dt = fenics.Constant(dt)
         
+        w = fenics.TrialFunction(self.W)
+        
         u, p, theta = fenics.split(w)
         
-        u_n, p_n, theta_n = fenics.split(w_n)
-        
         v, q, phi = fenics.TestFunctions(self.W)
+        
+        w_ = fenics.Function(self.W)
+
+        u_n, p_n, theta_n = fenics.split(w_n)
     
         Ra, Pr, Ste, C, K, g, gamma, mu_l = fenics.Constant(self.parameters['Ra']), fenics.Constant(self.parameters['Pr']), fenics.Constant(self.parameters['Ste']), fenics.Constant(self.parameters['C']), fenics.Constant(self.parameters['K']), fenics.Constant(self.parameters['g']), fenics.Constant(self.parameters['gamma']), fenics.Constant(self.parameters['mu_l'])
         
@@ -59,7 +63,11 @@ class FormFactory():
         
         heaviside_tanh = lambda theta, f_s, f_l: f_l + (f_s - f_l)/2.*(1. + fenics.tanh(a_s*(theta_s - theta)/R_s))
         
-        S = lambda theta : heaviside_tanh(theta, f_s=fenics.Constant(0.), f_l=fenics.Constant(1./Ste))
+        S_s = fenics.Constant(0.)
+        
+        S_l = fenics.Constant(1./Ste)
+        
+        S = lambda theta : heaviside_tanh(theta, f_s=S_s, f_l=S_l)
         
         F = (
             b(u, q) - gamma*p*q
@@ -67,61 +75,40 @@ class FormFactory():
             + C*theta*phi/dt - dot(u, grad(phi))*C*theta + dot(K/Pr*grad(theta), grad(phi)) - C*theta_n*phi/dt 
             + C*S(theta)*phi/dt - S(theta_n)*phi/dt
             )*fenics.dx
+
+        #w_k = fenics.Function(self.W)
             
-        return F
-
-
-    def make_newton_linearized_form(self, dt, w_n, w_k):
-    
-        dt = fenics.Constant(dt)
+        F = fenics.action(F, w_)
         
-        u_n, p_n, theta_n = fenics.split(w_n)
         
-        u_k, p_k, theta_k = fenics.split(w_k)
-    
+        # Jacobian per http://home.simula.no/~hpl/homepage/fenics-tutorial/release-1.0-nonabla/webm/nonlinear.html
         w_w = fenics.TrialFunction(self.W)
-    
-        u_w, p_w, theta_w = fenics.split(w_w)
         
-        v, q, phi = fenics.TestFunctions(self.W)
+        #u_w, p_w, theta_w = w_w.split()
         
-        Ra, Pr, Ste, C, K, g, gamma, mu_l = fenics.Constant(self.parameters['Ra']), fenics.Constant(self.parameters['Pr']), fenics.Constant(self.parameters['Ste']), fenics.Constant(self.parameters['C']), fenics.Constant(self.parameters['K']), fenics.Constant(self.parameters['g']), fenics.Constant(self.parameters['gamma']), fenics.Constant(self.parameters['mu_l'])
+        #u_k, p_k, theta_k = w_k.split()
         
-        m_B, ddtheta_m_B = self.m_B, self.ddtheta_m_B
-        
-        a_s, theta_s, R_s = fenics.Constant(self.regularization['a_s']), fenics.Constant(self.regularization['theta_s']), fenics.Constant(self.regularization['R_s'])
-        
-        heaviside_tanh = lambda theta, f_s, f_l: f_l + (f_s - f_l)/2.*(1. + fenics.tanh(a_s*(theta_s - theta)/R_s))
+        ddtheta_m_B = self.ddtheta_m_B
         
         ddtheta_heaviside_tanh = lambda theta, f_s, f_l: -(a_s*(fenics.tanh((a_s*(theta_s - theta))/R_s)**2 - 1.)*(f_l/2. - f_s/2.))/R_s
         
-        S_s = fenics.Constant(0.)
-        
-        S_l = fenics.Constant(1./Ste)
-        
-        S = lambda theta : heaviside_tanh(theta, f_s=S_s, f_l=S_l)
-        
         ddtheta_S = lambda theta : ddtheta_heaviside_tanh(theta, f_s=S_s, f_l=S_l)   
         
-        ddtheta_mu_l = 0.
+        ddtheta_mu_l = 0. # @todo Add variable viscosity
         
-        A = (
+        J = fenics.derivative(F, w_, w)
+        '''
+        J = (
             b(u_w, q) - gamma*p_w*q
-            + dot(u_w, v)/dt + c(u_w, u_k, v) + c(u_k, u_w, v) + a(mu_l, u_w, v) + a(ddtheta_mu_l*theta_w, u_k, v) + b(v, p_w) + dot(ddtheta_m_B(theta_k)*theta_w*g, v)
+            - (b(u_k, q) - gamma*p_k*q)
+            + dot(u_w, v)/dt + c(u_w, u_k, v) + c(u_k, u_w, v) + a(mu_l, u_w, v) + a(ddtheta_mu_l*theta_w, u_k, v) + b(v, p_w) + dot(ddtheta_m_B(theta_k)*theta_w*g, v) 
+            - (dot(u_k - u_n, v)/dt + c(u_k, u_k, v) + a(mu_l, u_k, v) + b(v, p_k) + dot(m_B(theta_k)*g, v))
             + theta_w*phi/dt - dot(u_k, grad(phi))*theta_w - dot(u_w, grad(phi))*theta_k + dot(K/Pr*grad(theta_w), grad(phi))
+            - ((theta_k - theta_n)*phi/dt - dot(u_k, grad(phi))*theta_k + dot(K/Pr*grad(theta_k), grad(phi)))
             + dot(ddtheta_S(theta_k)*theta_w, phi)/dt
+            - ((S(theta_k) - S(theta_n))*phi/dt)
             )*fenics.dx
+        '''
         
-        L = (
-            b(u_k, q) - gamma*p_k*q
-            + dot(u_k - u_n, v)/dt + c(u_k, u_k, v) + a(mu_l, u_k, v) + b(v, p_k) + dot(m_B(theta_k)*g, v)
-            + (theta_k - theta_n)*phi/dt - dot(u_k, grad(phi))*theta_k + dot(K/Pr*grad(theta_k), grad(phi))
-            + (S(theta_k) - S(theta_n))*phi/dt
-            )*fenics.dx  
+        return F, J
         
-        return A, L
-
-
-if __name__=='__main__':
-
-    initialize()
