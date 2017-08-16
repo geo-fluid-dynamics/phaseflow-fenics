b2d66d19bb3be3b4f6856de647fd8da0d76bfb35
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-08-16 11:29:53 +0200

    Added unsteady test

--- tests/test_ghia1982.py
+++ tests/test_ghia1982.py
@@ -48,6 +48,36 @@ def ghia1982_steady_lid_driven_cavity(automatic_jacobian=True):
             {'subspace': 2, 'value_expression': "1.", 'degree': 2, 'location_expression': bottom_left_corner, 'method': 'pointwise'}])
 
     verify_against_ghia1982(w, mesh)
+    
+    
+def unsteady_lid_driven_cavity(automatic_jacobian=True):
+
+    lid = 'near(x[1],  1.)'
+
+    fixed_walls = 'near(x[0],  0.) | near(x[0],  1.) | near(x[1],  0.)'
+
+    bottom_left_corner = 'near(x[0], 0.) && near(x[1], 0.)'
+
+    m = 20
+
+    w, mesh = phaseflow.run(
+        automatic_jacobian = automatic_jacobian,
+        mesh = fenics.UnitSquareMesh(fenics.dolfin.mpi_comm_world(), m, m, 'crossed'),
+        final_time = 10.,
+        time_step_bounds = (0.1, 0.1, 10.),
+        stop_when_steady = True,
+        mu_l = 0.01,
+        g = (0., 0.),
+        Ste = 1.e16,
+        output_dir='output/test_ghia1982_steady_lid_driven_cavity',
+        initial_values_expression = (lid, "0.", "0.", "1."),
+        boundary_conditions = [
+            {'subspace': 0, 'value_expression': ("1.", "0."), 'degree': 3, 'location_expression': lid, 'method': 'topological'},
+            {'subspace': 0, 'value_expression': ("0.", "0."), 'degree': 3, 'location_expression': fixed_walls, 'method': 'topological'},
+            {'subspace': 1, 'value_expression': "0.", 'degree': 2, 'location_expression': bottom_left_corner, 'method': 'pointwise'},
+            {'subspace': 2, 'value_expression': "1.", 'degree': 2, 'location_expression': bottom_left_corner, 'method': 'pointwise'}])
+
+    verify_against_ghia1982(w, mesh)
 
 
             
@@ -60,9 +90,16 @@ def test_ghia1982_steady_lid_driven_cavity_manualJ():
 
     ghia1982_steady_lid_driven_cavity(automatic_jacobian=False)
     
+
+def test_unsteady_lid_driven_cavity_manualJ():
+
+    unsteady_lid_driven_cavity(automatic_jacobian=False)
+    
     
 if __name__=='__main__':
 
     test_ghia1982_steady_lid_driven_cavity_autoJ()
 
     test_ghia1982_steady_lid_driven_cavity_manualJ()
+
+    test_unsteady_lid_driven_cavity_manualJ()
