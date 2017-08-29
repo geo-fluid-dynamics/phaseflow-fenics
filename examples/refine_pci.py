fa731e0e74dcb931305652c65bfb88f51aa03465
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-08-29 09:35:05 +0200

    Added example of refining initial pci

--- examples/refine_pci.py
+++ examples/refine_pci.py
@@ -1,77 +1,86 @@
 import fenics
-import sys
-import os.path
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
 import phaseflow
-
-
-def refine_pci():
-
-    theta_h = 0.5
-    
-    theta_c = -0.5
-    
-    R_s = 0.01
-    
-    mesh = fenics.UnitIntervalMesh(1)
-    
-    hot_boundary_refinement_cycles = 10
-
-    
-    ''' Refine mesh near hot boundary
-    The usual approach of using SubDomain and EdgeFunction isn't appearing to work
-    in 1D, so I'm going to just loop through the cells of the mesh and set markers manually.
-    '''
-    for i in range(hot_boundary_refinement_cycles):
-    
-        cell_markers = fenics.CellFunction("bool", mesh)
         
-        cell_markers.set_all(False)
-        
-        for cell in fenics.cells(mesh):
-        
-            found_hot_boundary = False
-        
-            for vertex in fenics.vertices(cell):
-            
-                if fenics.near(vertex.x(0), 0., fenics.dolfin.DOLFIN_EPS):
-                
-                    found_hot_boundary = True
-                    
-            if found_hot_boundary:
-            
-                cell_markers[cell] = True
+def refine_pci(Ste = 0.045,
+        Ra = 3.27e5,
+        Pr = 56.2,
+        mu_s = 1.e8,
+        theta_s = 0.1,
+        R_s = 0.05,
+        m = 10,
+        time_step_bounds = (1.e-3, 1.e-3, 1.e-3),
+        initial_pci_refinement_cycles = 6,
+        max_pci_refinement_cycles = 6,
+        output_dir='output/refine_pci',
+        start_time=0.,
+        end_time=1.,
+        nlp_divergence_threshold = 1.e12,
+        nlp_relaxation = 1.,
+        nlp_max_iterations = 30,
+        restart=False,
+        restart_filepath=''):
 
-                break # There should only be one such point.
-                
-        mesh = fenics.refine(mesh, cell_markers)
+    theta_hot = 1.
     
-    #
-    w = phaseflow.run(
-        output_dir = 'output/stefan_problem_refine_pci/',
-        output_format = 'table',
-        Pr = 1.,
-        Ste = 1.,
-        g = [0.],
-        mesh = mesh,
-        max_pci_refinement_cycles = 10,
+    theta_cold = -0.1
+
+    w, mesh = phaseflow.run(
+        Ste = Ste,
+        Ra = Ra,
+        Pr = Pr,
+        mu_s = mu_s,
+        mu_l = 1.,
+        mesh = fenics.UnitSquareMesh(m, m),
+        time_step_bounds = time_step_bounds,
+        start_time = start_time,
+        end_time = end_time,
+        output_times = ('all',),
+        stop_when_steady = True,
+        automatic_jacobian = False,
+        custom_newton = True,
+        regularization = {'a_s': 2., 'theta_s': theta_s, 'R_s': R_s},
+        initial_pci_refinement_cycles = initial_pci_refinement_cycles,
+        max_pci_refinement_cycles = max_pci_refinement_cycles,
+        nlp_max_iterations = nlp_max_iterations,
+        nlp_divergence_threshold = nlp_divergence_threshold,
+        nlp_relaxation = nlp_relaxation,
         initial_values_expression = (
             "0.",
             "0.",
-            "("+str(theta_h)+" - "+str(theta_c)+")*near(x[0],  0.) "+str(theta_c)),
+            "0.",
+            "("+str(theta_hot)+" - "+str(theta_cold)+")*(x[0] < 0.001) + "+str(theta_cold)),
         boundary_conditions = [
-            {'subspace': 0, 'value_expression': [0.], 'degree': 3, 'location_expression': "near(x[0],  0.) | near(x[0],  1.)", 'method': "topological"},
-            {'subspace': 2, 'value_expression': theta_h, 'degree': 2, 'location_expression': "near(x[0],  0.)", 'method': "topological"},
-            {'subspace': 2, 'value_expression': theta_c, 'degree': 2, 'location_expression': "near(x[0],  1.)", 'method': "topological"}],
-        regularization = {'a_s': 2., 'theta_s': 0.01, 'R_s': R_s},
-        newton_relative_tolerance = 1.e-4,
-        final_time = 0.01,
-        time_step_bounds = 0.0005,
-        linearize = False)
+            {'subspace': 0, 'value_expression': ("0.", "0."), 'degree': 3,
+                'location_expression': "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)",
+                'method': "topological"},
+            {'subspace': 2, 'value_expression': str(theta_hot), 'degree': 2,
+                'location_expression': "near(x[0],  0.)",
+                'method': "topological"},
+            {'subspace': 2, 'value_expression': str(theta_cold), 'degree': 2,
+                'location_expression': "near(x[0],  1.)",
+                'method': "topological"}],
+        output_dir = output_dir,
+        debug = True,
+        restart = restart,
+        restart_filepath = restart_filepath)
 
-        
-if __name__=='__main__':
+    return w, mesh
     
-    refine_pci()
+    
+def run_refine_pci():
+
+    for r in range(3):
+    
+        w, mesh = refine_pci(Ste = 1.,
+            theta_s = 0.,
+            R_s = 0.05,
+            m = 1,
+            end_time = 0.,
+            initial_pci_refinement_cycles = r,
+            max_pci_refinement_cycles = r,
+            output_dir = 'output/refine_pci_'+str(r))
+        
+    
+if __name__=='__main__':
+
+    run_refine_pci()
