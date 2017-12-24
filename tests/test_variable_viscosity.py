6c6f4054a0d31dcc9eaf955fe63c157e0af88015
Alex Zimmerman <zimmerman@aices.rwth-aachen.de>
2017-12-23 22:47:46 -0600

    Increased time step size

--- tests/test_variable_viscosity.py
+++ tests/test_variable_viscosity.py
@@ -69,7 +69,7 @@ def test_variable_viscosity__nightly():
     w, mesh = phaseflow.run(
         mesh = mesh,
         end_time = 20.,
-        time_step_size = 2.,
+        time_step_size = 4.,
         stop_when_steady = True,
         steady_relative_tolerance = 1.e-4,
         thermal_conductivity = 0.,
