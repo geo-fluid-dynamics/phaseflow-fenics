1db9ccaf99b5ee0b1ce0315207a3b8c1084e84fd
alexanderzimmerman <zimmerman@aices.rwth-aachen.de>
2018-01-07 16:12:27 +0100

    Reverted changes to regression test, which was edited by accident. Only the new example has the octadecane parameters

--- tests/test_melt_pcm.py
+++ tests/test_melt_pcm.py
@@ -4,7 +4,7 @@ from .context import phaseflow
 import fenics
 import scipy.optimize as opt
 
-T_f = 0.01
+T_f = 0.1
 
 def melt_toy_pcm(output_dir = "output/test_melt_toy_pcm/"):
     
@@ -12,7 +12,7 @@ def melt_toy_pcm(output_dir = "output/test_melt_toy_pcm/"):
     # Make the mesh.
     initial_mesh_size = 1
     
-    mesh = fenics.UnitSquareMesh(initial_mesh_size, initial_mesh_size)
+    mesh = fenics.UnitSquareMesh(initial_mesh_size, initial_mesh_size, 'crossed')
     
     initial_hot_wall_refinement_cycles = 6
     
@@ -47,7 +47,7 @@ def melt_toy_pcm(output_dir = "output/test_melt_toy_pcm/"):
     
     T_cold = -0.1
     
-    initial_pci_position = 1./float(initial_mesh_size)/2.**(initial_hot_wall_refinement_cycles - 1)
+    initial_pci_position = 0.001
     
     walls = "near(x[0],  0.) | near(x[0],  1.) | near(x[1], 0.) | near(x[1],  1.)"
     
@@ -56,22 +56,20 @@ def melt_toy_pcm(output_dir = "output/test_melt_toy_pcm/"):
     cold_wall = "near(x[0],  1.)"
     
     w, mesh = phaseflow.run(
-        stefan_number = 0.045,
-        rayleigh_number = 3.27e5,
-        prandtl_number = 56.2,
-        solid_viscosity = 1.e8,
+        stefan_number = 1.,
+        rayleigh_number = 1.e6,
+        prandtl_number = 0.71,
+        solid_viscosity = 1.e4,
         liquid_viscosity = 1.,
-        time_step_size = 1.,
-        end_time = 2.,
+        time_step_size = 1.e-3,
+        end_time = 0.02,
         stop_when_steady = True,
         temperature_of_fusion = T_f,
-        regularization_smoothing_factor = 0.05,
+        regularization_smoothing_factor = 0.025,
         adaptive = True,
         adaptive_metric = 'phase_only',
         adaptive_solver_tolerance = 1.e-4,
         nlp_relative_tolerance = 1.e-8,
-        nlp_max_iterations = 100,
-        nlp_relaxation = 1.,
         initial_values = fenics.interpolate(
             fenics.Expression(
                 ("0.", "0.", "0.", "(T_hot - T_cold)*(x[0] < initial_pci_position) + T_cold"),
