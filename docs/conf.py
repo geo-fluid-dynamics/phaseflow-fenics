d2200112cd5999401d49090aa9f029028952e88b
Alexander Zimmerman <zimmerman@aices.rwth-aachen.de>
2018-03-06 19:50:56 +0100

    Mock out modules with C dependencies, and no longer require them for install since this breaks readthedocs virtualenv

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,26 @@
 #
 import os
 import sys
+
+
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Mock out libraries with C dependencies ----------------------------------
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+    
+            return MagicMock()
+
+MOCK_MODULES = ["numpy", "h5py"]
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- Project information -----------------------------------------------------
