33d9f5bd431ddca1780baedf1db25c0bd68c9896
Alexander G. Zimmerman <alex.g.zimmerman@gmail.com>
2018-07-20 19:17:28 +0200

    Mock numpy but not h5py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     
             return MagicMock()
 
-MOCK_MODULES = ["fenics", "h5py"]
+MOCK_MODULES = ["fenics", "numpy"]
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
