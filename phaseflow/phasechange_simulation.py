643210f99038d9ae045c632b50ec1175f7c4a6ec
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-08-28 17:24:52 +0200

    Corrected parameter name to concentration_buoyancy_ratio

--- phaseflow/phasechange_simulation.py
+++ phaseflow/phasechange_simulation.py
@@ -16,7 +16,7 @@ class AbstractSimulation(phaseflow.simulation.AbstractSimulation):
         
         self.temperature_rayleigh_number = fenics.Constant(1., name = "Ra_T")
         
-        self.buoyancy_ratio = fenics.Constant(1., name = "R_rho")
+        self.concentration_buoyancy_ratio = fenics.Constant(1., name = "R_rho")
         
         self.prandtl_number = fenics.Constant(1., name = "Pr")
         
