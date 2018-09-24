7a230e7c709f3d303ec527b87895922f3bf165de
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-09-24 14:53:43 +0200

    Fix behavior of writing QoI table when calling run multiples times with a single simulation instance

--- phaseflow/cavity_freezing_simulation.py
+++ phaseflow/cavity_freezing_simulation.py
@@ -27,8 +27,6 @@ class CavityFreezingSimulation(
         
         self.cold_wall_temperature.assign(0.25)
         
-        self.timestep = 0
-        
         if setup_solver:
         
             self.solver.parameters["newton_solver"]["maximum_iterations"] = 12
@@ -136,7 +134,7 @@ class CavityFreezingSimulation(
         
         assert(self.temperature_rayleigh_number.__float__() > 1.e-8)
         
-        if self.timestep == 0:
+        if self.time == 0.:
         
             self.solve_steady_state_heat_driven_cavity()
             
@@ -190,16 +188,18 @@ class CavityFreezingSimulation(
                 
                 results_file.write("\n")
         
-        write_parameters()
-        
-        write_results()
+        if self.time == 0.:
+            
+            write_parameters()
+            
+            write_results()
         
         time_tolerance = 1.e-8
         
         while (self.time < (endtime - time_tolerance)):
             
             write_newline()
-                
+            
             write_parameters()
             
             self.solve_with_auto_regularization(
@@ -215,8 +215,6 @@ class CavityFreezingSimulation(
             if plot:
         
                 self.plot(savefigs = savefigs)
-            
-            self.timestep += 1
             
             self.advance()
             
